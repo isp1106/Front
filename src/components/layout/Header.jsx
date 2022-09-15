c27a6f897c3f8b8c6fb23fa446088c82d7eb5476
YANGDAYOUNG <jc1525@hanmail.net>
2022-09-15 15:39:33 +0900

    #7 design: header 디자인중

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -3,15 +3,26 @@ import { Link } from "react-router-dom";
 
 function Header() {
   return (
-    <div>
-      <div  className='border-green-700 border-4'>
-      <img src="assets/logoSmall.svg" alt="logo image" className='flex justify-start align-middle'/>
-
-      <img src="assets/search_icon.svg" alt="search" />
+    <header>
+      <div  className='border-green-700 border-4 flex item-center'>
+        <div>
+          <img src="assets/logoSmall.svg" alt="로고이미지" className='flex justify-start align-middle h-[15rem]'/>
+        </div>
+        <div>
+          <div>
+            <img src="assets/search_icon.svg" className='w-[30rem]' alt="검색" />
+          </div>
+          <div>
+            <img src="assets/shoppingBag_icon.svg" className='w-[30rem]' alt="장바구니" />
+          </div>
+        </div>
+        {/* <Link to="/cart" >
+          <img src="assets/shoppingBag_icon"alt="장바구니" />
+        </Link> */}
       </div>
    
 
-    </div>
+    </header>
   )
 }
 
