673c99161d2880d5d6cc586d79d599ef82f5f8d6
YANGDAYOUNG <jc1525@hanmail.net>
2022-09-15 15:08:52 +0900

    #7 error: 라우터 없음!!

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
+import Header from "./components/layout/Header"
+
+Header
 function App() {
-  return <div className="p-4 text-black text-2xl ">Hello</div>
+  return <div className="p-4 text-black text-2xl "><Header/></div>
 }
 
 export default App
--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
-    <div>Header</div>
+    <div>
+      <div  className='border-green-700 border-4'>
+      <img src="assets/logoSmall.svg" alt="logo image" className='flex justify-start align-middle'/>
+
+      <img src="assets/search_icon.svg" alt="search" />
+      </div>
+   
+
+    </div>
   )
 }
 
