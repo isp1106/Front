a24cde1f18b12a283c7aa194546af141900911a3
whilini <whi0306@naver.com>
2022-09-17 02:07:52 +0900

    feat: Today Lank component without design

--- src/components/Main/TodayLank/Card.jsx
+++ src/components/Main/TodayLank/Card.jsx
@@ -1,5 +1,22 @@
 import React from 'react'
+import heartOn from '/public/assets/heart-on.svg'
+import heartOff from '/public/assets/heart.svg'
 
-export const Card = () => {
-  return <div>Card</div>
+export const Card = ({ data }) => {
+  const { userId, profile, liked } = data
+  return (
+    <div>
+      <div>
+        <img src={profile} alt={userId} />
+      </div>
+      <div>
+        <div>{userId}</div>
+        {liked ? (
+          <img src={heartOn} alt="heart on" />
+        ) : (
+          <img src={heartOff} alt="heart off" />
+        )}
+      </div>
+    </div>
+  )
 }
--- src/components/Main/TodayLank/index.jsx
+++ src/components/Main/TodayLank/index.jsx
@@ -1,5 +1,38 @@
 import React from 'react'
+import { Card } from './Card'
 
 export const TodayLank = () => {
-  return <div>index</div>
+  const lankUsers = [
+    {
+      userId: 'iHxxx_you',
+      profie:
+        'https://image.msscdn.net/images/style/detail/29026/detail_29026_1_500.jpg',
+      liked: false,
+    },
+    {
+      userId: 'xxxgdragonxxx',
+      profie: 'https://image.musinsa.com/mfile_s01/_lookbook/list632286b977ba4',
+      liked: true,
+    },
+    {
+      userId: 'wh._l._n._peach',
+      profie: 'https://www.musinsa.com/app/goods/1543796',
+      liked: true,
+    },
+    {
+      userId: 'lettttsgggo',
+      profie:
+        'https://image.msscdn.net/images/prd_img/20220830/2755582/detail_2755582_2_500.jpg',
+      liked: false,
+    },
+  ]
+  return (
+    <div>
+      <div>
+        {lankUsers.map((user) => (
+          <Card data={user} />
+        ))}
+      </div>
+    </div>
+  )
 }
