974841243566ff45bb713e7a0b34f22261c5a2b3
YANGDAYOUNG <jc1525@hanmail.net>
2022-09-16 15:02:04 +0900

    #21 fix: 필요 없는 props 삭제 및 className 정리

--- src/components/layout/Header/Logo.jsx
+++ src/components/layout/Header/Logo.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import { ReactComponent as LogoImg } from '/public/assets/logoSmall.svg'
-const Logo = (hideLogo) => {
-  console.log('hid', hideLogo)
+const Logo = () => {
   return (
     <h1 className="flex items-center h-9 ">
-      <LogoImg className={'flex h-8 items-center '} />
+      <LogoImg className="flex h-8 items-center" />
     </h1>
   )
 }
