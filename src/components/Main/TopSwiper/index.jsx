4248134f0c439a3788273a6793c89b7da49c0c80
YANGDAYOUNG <jc1525@hanmail.net>
2022-09-16 15:00:23 +0900

    #9 feat: import 추가

--- src/components/Main/TopSwiper/index.jsx
+++ src/components/Main/TopSwiper/index.jsx
@@ -1,7 +1,36 @@
 import React from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react'
 
-function index() {
-  return <div>index</div>
+//https://ifh.cc/g/6aO2yl.jpg
+
+const TopSwiper = () => {
+  return (
+    <div>
+      <Swiper
+        spaceBetween={50}
+        slidesPerView={3}
+        onSlideChange={() => console.log('slide change')}
+        onSwiper={(swiper) => console.log(swiper)}
+      >
+        <SwiperSlide>
+          <img src="https://ifh.cc/g/6aO2yl.jpg" alt="슬라이드1" />
+          <p>1</p>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://ifh.cc/g/6aO2yl.jpg" alt="슬라이드1" />
+          <p>2</p>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://ifh.cc/g/6aO2yl.jpg" alt="슬라이드1" />
+          <p>3</p>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://ifh.cc/g/6aO2yl.jpg" alt="슬라이드1" />
+          <p>4</p>
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  )
 }
 
-export default index
+export default TopSwiper
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import TodayRecommended from '../../components/Main/TodayRecommended'
+import TopSwiper from '../../components/Main/TopSwiper'
 
 const Home = () => {
   return (
     <div>
       <TodayRecommended />
+      <TopSwiper />
     </div>
   )
 }
