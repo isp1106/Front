f883b6665f6af68b0f7aeeac23480d8cfd9534d6
0seo8 <96939562+0seo8@users.noreply.github.com>
2022-09-17 12:03:18 +0900

    feat: 뒤로가기 컴포넌트 분리

--- src/components/common/GoBack.jsx
+++ src/components/common/GoBack.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { ReactComponent as GoBackIcon } from '/public/assets/back.svg'
+
 import { useNavigate } from 'react-router-dom'
 const GoBack = () => {
   const navigate = useNavigate()
