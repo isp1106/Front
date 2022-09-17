985ebda802b72a0aee865921bfef9150df75961d
0seo8 <96939562+0seo8@users.noreply.github.com>
2022-09-17 15:56:27 +0900

    design: finish Component

--- src/pages/SignUp/SignUpPw.jsx
+++ src/pages/SignUp/SignUpPw.jsx
@@ -1,22 +1,22 @@
 import React from 'react'
-import Button from '../../components/common/Button'
-import Title from '../../components/SignUp/Title'
 import NextBtn from '../../components/SignUp/NextBtn'
-const SignUpIdForm = () => {
+import Title from '../../components/SignUp/Title'
+import Button from '../../components/common/Button'
+const SignUpPw = () => {
   return (
     <>
-      <Title title="아이디" text="생성" />
+      <Title title="비밀번호" text="생성" />
       <div className="mt-36">
         <label
           htmlFor="loginJoinMembershipId"
           className="inline-block mt-4 mb-2 leading-snug"
         >
-          아이디
+          비밀번호
           <span className="inline-block overflow-hidden w-[4px] h-[4px] mt-[4px] rounded-full bg-red-600 indent-[999px] align-text-top ml-1">
             필수 입력
           </span>
         </label>
-        <div className="flex box-border h-[50px] border border-neutral-200 rounded justify-between items-center">
+        <div className="flex flex-col mb-2 box-border h-[50px] border border-neutral-200 rounded justify-between items-center">
           <input
             type="text"
             id="loginJoinMembershipId"
@@ -29,14 +29,24 @@ const SignUpIdForm = () => {
             {/* svg */}
           </button>
         </div>
+        <div className="flex flex-col mb-2 box-border h-[50px] border border-neutral-200 rounded justify-between items-center">
+          <input
+            type="text"
+            id="loginJoinMembershipId"
+            placeholder="비밀번호 확인을 입력해주세요"
+            maxLength="11"
+            minLength="6"
+            className="pl-3 h-[48px] border-none flex-initial box-border w-full py-[12px] rounded text-[14px] transition shadow-white"
+          />
+          <button type="button" className="login-input__button-clear indent-96">
+            {/* svg */}
+          </button>
+        </div>
         <p className="login-input__validation">{/* 오류내용 */}</p>
-        <Button classprop=" border border-black-100 mt-6 gap-2">
-          중복확인
-        </Button>
       </div>
-      <NextBtn next="pwform" />
+      <NextBtn next="infoform" />
     </>
   )
 }
 
-export default SignUpIdForm
+export default SignUpPw
--- src/pages/SignUp/UserInfo.jsx
+++ src/pages/SignUp/UserInfo.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import NextBtn from '../../components/SignUp/NextBtn'
+import Title from '../../components/SignUp/Title'
 
 const UserInfo = () => {
   return (
     <>
+      <Title title="정보" text="입력" />
       UserInfo
       <NextBtn next="finish" />
     </>
