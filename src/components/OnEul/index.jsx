d8b17e7614a9232ef8931a8ecf57849deab5134a
0seo8 <96939562+0seo8@users.noreply.github.com>
2022-09-18 00:21:24 +0900

    design: picture

--- src/components/OnEul/index.jsx
+++ src/components/OnEul/index.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Header from './Header'
+import Picture from './Picture'
 const OnEul = () => {
   return (
     <div>
       <Header />
+      <Picture />
     </div>
   )
 }
