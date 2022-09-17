2ab5f0b95e4bfb3ec951933ae4c32fef03721fa0
0seo8 <96939562+0seo8@users.noreply.github.com>
2022-09-18 00:03:31 +0900

    design: header

--- src/components/OnEul/index.jsx
+++ src/components/OnEul/index.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
-
+import Header from './Header'
 const OnEul = () => {
-  return <div>OnEul</div>
+  return (
+    <div>
+      <Header />
+    </div>
+  )
 }
 
 export default OnEul
