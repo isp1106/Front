2a394f4b7653480270df5b0ad127278878d1d7dc
0seo8 <96939562+0seo8@users.noreply.github.com>
2022-09-17 13:50:16 +0900

    feat: navBottom 조건 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, useLocation } from 'react-router-dom'
 import './App.css'
 import Home from './pages/Home'
 import Like from './pages/Like'
@@ -6,11 +6,13 @@ import Onul from './pages/Onul'
 import Login from './pages/Login'
 import Cart from './pages/Cart'
 import MyPage from './pages/MyPage'
-import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
 import Category from './pages/Category'
 import NavBottom from './components/layout/NavBottom'
 
 function App() {
+  const location = useLocation()
+  const currentPath = location.pathname
   return (
     <div className="pb-[64px]">
       <Routes>
@@ -20,10 +22,10 @@ function App() {
         <Route path="/like" element={<Like />} />
         <Route path="/my" element={<MyPage />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
         <Route path="/cart" element={<Cart />} />
       </Routes>
-      <NavBottom />
+      {currentPath !== '/signup' && <NavBottom />}
     </div>
   )
 }
