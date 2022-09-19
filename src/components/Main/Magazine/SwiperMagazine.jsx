5b0db41868e00cafc161b25c9d4788b82f7e63f3
0seo8 <96939562+0seo8@users.noreply.github.com>
2022-09-19 17:30:37 +0900

    fix: key값 에러 수정

--- src/components/Main/Magazine/SwiperMagazine.jsx
+++ src/components/Main/Magazine/SwiperMagazine.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import { ReactComponent as LikeWhiteIcon } from '/public/assets/like-white.svg'
 import { magazineWoman as list } from '../../../dummy/magazine'
 
@@ -8,7 +8,7 @@ const SwiperMagazine = () => {
   return (
     <div className="flex gap-4 overflow-x-scroll py-[39px]">
       {list.map((item, index) => (
-        <>
+        <React.Fragment key={index}>
           <img src={item.url} width="150" />
           <button
             className="absolute top-4 right-4"
@@ -17,7 +17,7 @@ const SwiperMagazine = () => {
             {like ? <LikeWhiteIcon /> : <LikeWhiteIcon />}
             {/* 나중에 아이콘넣기 */}
           </button>
-        </>
+        </React.Fragment>
       ))}
     </div>
   )
