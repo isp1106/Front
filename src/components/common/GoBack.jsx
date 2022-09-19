9fcc4b720c6edc4ccfc3b5dc24f47d9e114d925b
whilini <whi0306@naver.com>
2022-09-20 02:47:59 +0900

    feat: Assign terms page design

--- src/components/common/GoBack.jsx
+++ src/components/common/GoBack.jsx
@@ -5,7 +5,10 @@ import { useNavigate } from 'react-router-dom'
 const GoBack = () => {
   const navigate = useNavigate()
   return (
-    <div className="pt-3 pb-3 pl-3" onClick={() => navigate(-1)}>
+    <div
+      className="pt-12 pb-3 pl-3 fixed overflow-hidden bg-white w-full"
+      onClick={() => navigate(-1)}
+    >
       <GoBackIcon />
     </div>
   )
