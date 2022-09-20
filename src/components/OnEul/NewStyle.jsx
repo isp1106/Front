75bb43307df200cde24baee6ce313b69e8f1e237
whilini <whi0306@naver.com>
2022-09-21 03:00:31 +0900

    feat: design NewStyle component in ONeul page with grid

--- src/components/OnEul/NewStyle.jsx
+++ src/components/OnEul/NewStyle.jsx
@@ -21,18 +21,20 @@ const NewStyle = ({ category }) => {
       return
   }
   return (
-    <div className="mt-[35px]">
+    <div className="my-[35px]">
       <h4 className="font-bold mb-5 pl-5">오늘의 뉴 스타일</h4>
-      <div className="product__list">
+      <div className="w-full grid grid-cols-2 border-t-2 border-primary overflow-hidden">
         {list.map((item, index) => (
-          <div key={index} className="item__list">
-            <div
-              className="item w-[50vw] h-[50vw] h bg-cover"
-              style={{
-                backgroundImage: `url(${item.imgUrl})`,
-              }}
-            ></div>
-            <div className="flex gap-3 py-2 pl-2">
+          <div
+            key={index}
+            className="w-full [&:nth-child(2n)]:border-l-2 border-primary box-content"
+          >
+            <div className="flex justify-center align-items-center">
+              <div className="w-full bg-cover overflow-hidden relative justify-center after:content('') after:block after:pb-[100%]">
+                <img src={item.imgUrl} alt={item.imgUrl} className="absolute" />
+              </div>
+            </div>
+            <div className="flex gap-3 py-2 pl-2 border-y-2 border-primary">
               <div
                 className="w-[43px] h-[43px] bg-cover rounded-lg "
                 style={{
