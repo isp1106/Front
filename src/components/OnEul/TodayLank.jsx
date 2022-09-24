55b06483c54d1215bbdc957b90667520035ed479
0seo8 <96939562+0seo8@users.noreply.github.com>
2022-09-24 20:24:47 +0900

    feat: 라우터 설정

--- src/components/OnEul/TodayLank.jsx
+++ src/components/OnEul/TodayLank.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 import HeartIcon from '../common/HeartIcon'
 import { TodayLanklist as list } from '../../dummy/main'
 //더미 데이터
 
 const TodayLank = () => {
+  const navigate = useNavigate()
   return (
     <div className="mt-10 pl-5">
       <h4 className="font-bold mb-5 text-xl">오늘의 랭크</h4>
       <div className="flex gap-[19px] overflow-x-scroll">
         {list.map((item, index) => (
-          <div key={index}>
+          <div key={index} onClick={() => navigate('/oneul/detail')}>
             <div
               className="new-style w-[225px] h-[260px] bg-cover relative"
               style={{
