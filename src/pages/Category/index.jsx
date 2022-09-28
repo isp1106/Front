859291ce46b194280cb54f79d08a5184830569df
YANGDAYOUNG <jc1525@hanmail.net>
2022-09-29 00:04:34 +0900

    #31 feat: 그리드로 적용..

--- src/pages/Category/index.jsx
+++ src/pages/Category/index.jsx
@@ -1,8 +1,12 @@
 import React, { useState } from 'react'
-import { Link, Element } from 'react-scroll'
+import { Link as ScrollLink, Element } from 'react-scroll'
+import { Link, useNavigate, useLocation } from 'react-router-dom'
 import CategoryItem from './CategoryItem'
-import { CATEGORY } from '../../dummy/constants'
+import { CATEGORY } from '../../dummy/constantsfix'
 import FastionTab from './FastionTab'
+import { ReactComponent as SearchIcon } from '/public/assets/search_icon.svg'
+import { ReactComponent as CartIcon } from '/public/assets/bag.svg'
+
 import './Category.css'
 
 const Category = () => {
@@ -13,8 +17,133 @@ const Category = () => {
   }
   return (
     <div>
-      <nav className="border-t-2 border-primary flex flex-col ">
-        {/* {category === '뷰티' && (
+      <nav className="grid grid-cols-3 h-full place-items-stretch  ">
+        <div className="col-span-3">
+          header
+          <div>
+            <input type="text" />
+            <SearchIcon />
+            <Link to="/cart">
+              <CartIcon onClick={() => navigate('/cart')} />
+            </Link>
+          </div>
+        </div>
+        {true && <div className="col-span-3">brand kdkdkdkddkkd</div>}
+        <div className="border-t-2 border-primary h-screen">
+          <ul className="flex w-28 flex-col fixed">
+            {CATEGORY.map(({ TopCategory }) => {
+              return (
+                <li className=" h-12" key={TopCategory}>
+                  <ScrollLink
+                    activeClass="active"
+                    className="text-xs font-bold px-5 py-3 block w-full h-full "
+                    to={TopCategory}
+                    spy={true}
+                    smooth={true}
+                    duration={500}
+                    onClick={() => {
+                      setCategory(TopCategory)
+                    }}
+                  >
+                    {TopCategory}
+                  </ScrollLink>
+                </li>
+              )
+            })}
+          </ul>
+        </div>
+        <div className="col-span-2">
+          {CATEGORY.map(({ TopCategory, subCategory }, index) => {
+            return (
+              <div key={(index, TopCategory)}>
+                {TopCategory === '패션' && (
+                  <Element
+                    name={TopCategory}
+                    className="box-border border-x-2 border-primary h-full bg-[#f3f3f3] "
+                  >
+                    <FastionTab
+                      openWomenTab={openWomenTab}
+                      getTabData={getTabData}
+                    />
+                    <h3 className="text-sm  h-12  font-bold uppercase text-primary px-5 py-3 block border-b-2  border-primary ">
+                      {TopCategory}
+                    </h3>
+                    {subCategory.map(({ name, obj }, index) => {
+                      return (
+                        <div key={index}>
+                          <h4 className=" h-12 text-sm font-bold uppercase text-primary px-5 py-3 block border-b-2  border-primary sticky top-12 bg-white">
+                            {name}
+                          </h4>
+                          <div className="grid grid-cols-3 place-items-center border-b-2  border-primary">
+                            {openWomenTab
+                              ? obj.women.map((item, index) => (
+                                  <CategoryItem key={index} item={item} />
+                                ))
+                              : obj.men.map((item, index) => (
+                                  <CategoryItem key={index} item={item} />
+                                ))}
+                          </div>
+                        </div>
+                      )
+                    })}
+                  </Element>
+                )}
+                {TopCategory == '뷰티' && (
+                  <Element
+                    name={TopCategory}
+                    className="border-x-2 border-primary h-full bg-[#f3f3f3] "
+                  >
+                    <h3 className="text-sm  h-12  font-bold uppercase text-primary px-5 py-3 block border-b-2  border-primary sticky top-0  bg-white z-10 ">
+                      {TopCategory}
+                    </h3>
+
+                    {subCategory.slice(1).map(({ name, list }, index) => {
+                      return (
+                        <div key={index}>
+                          <h4 className=" h-12  text-sm font-bold uppercase text-primary px-5 py-3 block border-b-2  border-primary sticky top-12 bg-white">
+                            {name}
+                          </h4>
+                          <div className="grid grid-cols-3 place-items-center border-b-2  border-primary">
+                            {list.map((item, index) => (
+                              <CategoryItem key={index} item={item} />
+                            ))}
+                          </div>
+                        </div>
+                      )
+                    })}
+                  </Element>
+                )}
+                {TopCategory !== '패션' && TopCategory !== '뷰티' && (
+                  <Element
+                    name={TopCategory}
+                    className="border-x-2 border-primary h-full bg-[#f3f3f3] "
+                  >
+                    <h3 className="text-sm  h-12  font-bold uppercase text-primary px-5 py-3 block border-b-2  border-primary sticky top-0  bg-white z-10 ">
+                      {TopCategory}
+                    </h3>
+                    {subCategory.map(({ name, list }, index) => {
+                      return (
+                        <div key={index}>
+                          <h4 className=" h-12  text-sm font-bold uppercase text-primary px-5 py-3 block border-b-2  border-primary sticky top-12 bg-white">
+                            {name}
+                          </h4>
+                          <div className="grid grid-cols-3 place-items-center border-b-2  border-primary">
+                            {list.map((item, index) => (
+                              <CategoryItem key={index} item={item} />
+                            ))}
+                          </div>
+                        </div>
+                      )
+                    })}
+                  </Element>
+                )}
+              </div>
+            )
+          })}
+        </div>
+      </nav>
+      {/* <nav className="border-t-2 border-primary flex flex-col ">
+        {category === '뷰티' && (
           <div className="">
             <h5 className="block p-5 text-sm font-bold">브랜드</h5>
             <div className="px-5">
@@ -51,8 +180,7 @@ const Category = () => {
               })}
             </div>
           </div>
-        
-        )} */}
+        )}
         <div className="flex ">
           <ul className="flex w-28 flex-col fixed">
             {CATEGORY.map(({ TopCategory }) => {
@@ -127,7 +255,6 @@ const Category = () => {
                             <h4 className=" h-12  text-sm font-bold uppercase text-primary px-5 py-3 block border-b-2  border-primary sticky top-12 bg-white">
                               {name}
                             </h4>
-                            {/* h4 즉 2.2.1 제목의 경우 무신사에서도 그냥 sticky처리 안하는거 같은뎅..!!  */}
                             <div className="grid grid-cols-3 place-items-center border-b-2  border-primary">
                               {list.map((item, index) => (
                                 <CategoryItem key={index} item={item} />
@@ -167,7 +294,7 @@ const Category = () => {
             })}
           </div>
         </div>
-      </nav>
+      </nav> */}
     </div>
   )
 }
