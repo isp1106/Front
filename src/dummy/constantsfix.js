fd44f428a774cbe888db7d8d8ac5dad7e00f704a
YANGDAYOUNG <jc1525@hanmail.net>
2022-09-28 13:42:32 +0900

    #31 feat: 카테고리 데이터 devleop에 추가

---
+++ src/dummy/constantsfix.js
@@ -0,0 +1,251 @@
+export const CATEGORY = [
+  {
+    TopCategory: '패션',
+    subCategory: [
+      {
+        name: '의류',
+        imgSrc:
+          'https://whoisnerdy.com/web/product/big/202206/6655a56cb52cf635371b35292e2deb7f.png',
+        obj: {
+          women: [
+            '전제',
+            '상의',
+            '하의',
+            '원피스',
+            '아우터',
+            '언더웨어',
+            '셋업',
+            '기타',
+          ],
+          men: [
+            '전제',
+            '상의',
+            '하의',
+            '점프수트',
+            '아우터',
+            '언더웨어',
+            '셋업',
+            '기타',
+          ],
+        },
+      },
+      {
+        name: '가방',
+        imgSrc:
+          'https://static.goldengoose.com/image/upload/w_610,c_scale,dpr_1.0,f_auto/v1637677473/Style/ECOMM/GMA00230.A000340-35674',
+        obj: {
+          women: [
+            '전제',
+            '백팩',
+            '숄더백',
+            '토트백',
+            '에코백',
+            '보스턴 백',
+            '웨이스트백',
+            '클러치백',
+            '캐리어',
+            '소품',
+            '지갑',
+            '파우치',
+          ],
+          men: [
+            '전제',
+            '백팩',
+            '숄더백',
+            '토트백',
+            '에코백',
+            '보스턴 백',
+            '웨이스트백',
+            '클러치백',
+            '캐리어',
+            '소품',
+            '지갑',
+            '파우치',
+          ],
+        },
+      },
+      {
+        name: '신발',
+        imgSrc:
+          'https://whoisnerdy.com/web/product/big/202206/6655a56cb52cf635371b35292e2deb7f.png',
+        obj: {
+          women: [
+            '전제',
+            '힐',
+            '단화',
+            '부츠',
+            '워커',
+            '운동화',
+            '샌들',
+            '슬리퍼',
+          ],
+          men: [
+            '전제',
+            '힐',
+            '단화',
+            '부츠',
+            '워커',
+            '운동화',
+            '샌들',
+            '슬리퍼',
+          ],
+        },
+      },
+      {
+        name: '엑세사리',
+        imgSrc:
+          'https://whoisnerdy.com/web/product/big/202206/6655a56cb52cf635371b35292e2deb7f.png',
+        obj: {
+          women: ['전제', '귀걸이', '목걸이', '반지', '팔찌', '발찌'],
+          men: ['전제', '귀걸이', '목걸이', '반지', '팔찌', '발찌'],
+        },
+      },
+    ],
+  },
+  {
+    TopCategory: '코스매',
+    subCategory: [
+      {
+        name: '브랜드',
+        list: [
+          {
+            subCategoryName: 'Dr.esthe',
+            imgSrc:
+              'https://stylenanda.com/web/product/medium/20191206/dd268620246d217d50062844c4ef2693.jpg',
+          },
+          {
+            subCategoryName: 'Etude House',
+            imgSrc:
+              'https://stylenanda.com/web/product/medium/20191206/dd268620246d217d50062844c4ef2693.jpg',
+          },
+          {
+            subCategoryName: '3CE',
+            imgSrc:
+              'https://stylenanda.com/web/product/medium/20191206/dd268620246d217d50062844c4ef2693.jpg',
+          },
+          {
+            subCategoryName: 'CLIO',
+            imgSrc:
+              'https://stylenanda.com/web/product/medium/20191206/dd268620246d217d50062844c4ef2693.jpg',
+          },
+          {
+            subCategoryName: 'HERA',
+            imgSrc:
+              'https://stylenanda.com/web/product/medium/20191206/dd268620246d217d50062844c4ef2693.jpg',
+          },
+        ],
+      },
+      {
+        name: '스킨케어',
+        imgSrc:
+          'https://assets.burberry.com/is/image/Burberryltd/B2DA4233-4047-4295-B36B-F1E651DCEFAF?$BBY_V2_SL_1x1$&wid=1251&hei=1251',
+        list: [
+          '전체',
+          '스킨토너',
+          '클렌징',
+          '로션',
+          '에센스',
+          '세럼',
+          '엠플',
+          '마스크',
+        ],
+      },
+      {
+        name: '메이크업',
+        imgSrc:
+          'https://assets.burberry.com/is/image/Burberryltd/B2DA4233-4047-4295-B36B-F1E651DCEFAF?$BBY_V2_SL_1x1$&wid=1251&hei=1251',
+        list: ['전체', '포인트 메이크업', '베이스 메이크업', '기타'],
+      },
+      {
+        name: '헤어',
+        imgSrc:
+          'https://assets.burberry.com/is/image/Burberryltd/B2DA4233-4047-4295-B36B-F1E651DCEFAF?$BBY_V2_SL_1x1$&wid=1251&hei=1251',
+        list: ['전체', '샴푸/린스', '탈모케어', '스타일링', '기타'],
+      },
+      {
+        name: '향수',
+        imgSrc:
+          'https://assets.burberry.com/is/image/Burberryltd/B2DA4233-4047-4295-B36B-F1E651DCEFAF?$BBY_V2_SL_1x1$&wid=1251&hei=1251',
+
+        list: ['전체', '여성향수', '남성향수', '캔들', '디퓨저', '룸스프레이'],
+      },
+      {
+        name: '이너뷰티',
+        list: ['전체', '다이어트', '비타민', '건강', '가글'],
+        imgSrc:
+          'https://assets.burberry.com/is/image/Burberryltd/B2DA4233-4047-4295-B36B-F1E651DCEFAF?$BBY_V2_SL_1x1$&wid=1251&hei=1251',
+      },
+    ],
+  },
+  {
+    TopCategory: '헬스',
+    subCategory: [
+      {
+        name: '스포츠웨어',
+        imgSrc:
+          'https://assets.burberry.com/is/image/Burberryltd/B2DA4233-4047-4295-B36B-F1E651DCEFAF?$BBY_V2_SL_1x1$&wid=1251&hei=1251',
+
+        list: ['전체', '상의', '하의', '기타'],
+      },
+      {
+        name: '레저',
+        imgSrc:
+          'https://assets.burberry.com/is/image/Burberryltd/B2DA4233-4047-4295-B36B-F1E651DCEFAF?$BBY_V2_SL_1x1$&wid=1251&hei=1251',
+
+        list: ['전체', '캠핑', '수영', '골프', '기타'],
+      },
+    ],
+  },
+
+  {
+    TopCategory: '인테리어',
+    subCategory: [
+      {
+        name: '가구',
+        imgSrc:
+          'http://www.basicgagu.com/shopimages/83kjh/0110070001812.jpg?1611730476',
+        list: ['전체', '책생', '의자', '침대', '기타'],
+      },
+      {
+        name: '조명',
+        imgSrc:
+          'https://royaldesign.kr/image/9/tradition-milk-spare-glass-white-0?w=1920&quality=80',
+        list: ['전체', '색상', '리빙', '침대', '기타'],
+      },
+    ],
+  },
+  {
+    TopCategory: '리빙',
+    subCategory: [
+      {
+        name: '키친',
+        imgSrc:
+          'https://assets.burberry.com/is/image/Burberryltd/B2DA4233-4047-4295-B36B-F1E651DCEFAF?$BBY_V2_SL_1x1$&wid=1251&hei=1251',
+        list: ['전체', '색상', '리빙', '침대', '기타'],
+      },
+      {
+        name: '패브릭',
+        imgSrc:
+          'https://assets.burberry.com/is/image/Burberryltd/B2DA4233-4047-4295-B36B-F1E651DCEFAF?$BBY_V2_SL_1x1$&wid=1251&hei=1251',
+        list: ['전체', '색상', '리빙', '침대', '기타'],
+      },
+    ],
+  },
+  {
+    TopCategory: 'K-컬처',
+    subCategory: [
+      {
+        name: '아트',
+        imgSrc:
+          'https://assets.burberry.com/is/image/Burberryltd/B2DA4233-4047-4295-B36B-F1E651DCEFAF?$BBY_V2_SL_1x1$&wid=1251&hei=1251',
+        list: ['전체', '공예품', '굿즈', '아트프린트', '아트북', '기타'],
+      },
+      {
+        name: '문구',
+        imgSrc:
+          'https://assets.burberry.com/is/image/Burberryltd/B2DA4233-4047-4295-B36B-F1E651DCEFAF?$BBY_V2_SL_1x1$&wid=1251&hei=1251',
+        list: ['전체', '공예품', '굿즈', '아트프린트', '아트북', '기타'],
+      },
+    ],
+  },
+]
