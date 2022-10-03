c13739f968d26d299c35966d453cbadc7acb3ef2
0seo8 <96939562+0seo8@users.noreply.github.com>
2022-10-04 03:14:37 +0900

    feat: 상품등록페이지

--- src/components/My/QnAWrite/index.jsx
+++ src/components/My/QnAWrite/index.jsx
@@ -1,7 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { types } from '../../../utils/questions'
+import Type from './Type'
+import Content from './Content'
+import AddPicture from './AddPicture'
+import { useEffect } from 'react'
 
 const index = () => {
-  return <div>index</div>
+  const [count, setCount] = useState(0)
+  const [userValue, setUserValue] = useState({
+    type: types[0],
+    title: '',
+    content: '',
+    thumbnailBase64: '',
+  })
+  const onChangeHandler = (e) => {
+    const { name, value } = e.target
+    setUserValue({
+      ...userValue,
+      [name]: value,
+    })
+    name === 'content' && setCount(value.length)
+  }
+  const onChangeCheckedHandler = (idx) => {
+    setUserValue({
+      ...userValue,
+      type: types[idx],
+    })
+  }
+
+  const selectThumbnail = (e) => {
+    const fileReader = new FileReader()
+    fileReader.readAsDataURL(e.target.files[0])
+    fileReader.addEventListener('load', () => {
+      console.log(fileReader.result)
+    })
+  }
+
+  useEffect(() => {
+    console.log(userValue)
+  }, [userValue])
+
+  return (
+    <div className="px-5">
+      <Type
+        types={types}
+        userValue={userValue}
+        onChangeCheckedHandler={onChangeCheckedHandler}
+      />
+      <Content count={count} onChangeHandler={onChangeHandler} />
+      <AddPicture selectThumbnail={selectThumbnail} />
+    </div>
+  )
 }
 
 export default index
