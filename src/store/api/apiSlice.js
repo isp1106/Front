bbd33f8a67077db44865b4c347243fd3a7a1bb91
YANGDAYOUNG <jc1525@hanmail.net>
2022-10-04 21:48:42 +0900

    #77 feat: 기본 설정중..

--- src/store/api/apiSlice.js
+++ src/store/api/apiSlice.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //기본 createApi
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { baseQueryWithReauth } from './baseQuery'
@@ -6,7 +5,6 @@ export const apiSlice = createApi({
   baseQuery: baseQueryWithReauth,
   endpoints: (builder) => ({}),
 })
-=======
 // import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 // const baseQuery = fetchBaseQuery({
@@ -25,4 +23,3 @@ export const apiSlice = createApi({
 //   baseQuery,
 //   endpoints: (builder) => ({}),
 // })
->>>>>>> 559a89fd613b561d0cb3c772aa6caf2ff77061fc
