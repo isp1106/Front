b8d8922114a2af8fc59df80ee78b7b0b3d8eac6d
0seo8 <96939562+0seo8@users.noreply.github.com>
2022-10-06 00:30:08 +0900

    fix: api data forData로 변경

--- src/components/My/QnAWrite/index.jsx
+++ src/components/My/QnAWrite/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useMemo, useCallback, useEffect } from 'react'
+import React, { useState, useMemo, useRef, useEffect } from 'react'
 import { types } from '../../../utils/questions'
 import Type from './Type'
 import Content from './Content'
@@ -7,6 +7,7 @@ import QnABtn from './QnABtn'
 import Modal from '../../common/Modal'
 import CloseIcon from '../../common/CloseIcon'
 import useModalControl from '../../../hook/useModalControl'
+import { useAddQuestionMutation } from '../../../store/api/questionSlice'
 
 const index = () => {
   const [count, setCount] = useState(0)
@@ -14,11 +15,13 @@ const index = () => {
     type: types[0],
     title: null,
     content: null,
+    answerYn: true,
+    password: null,
   })
   const [imageFile, setImageFile] = useState([])
-  const [serverFile, setServerFile] = useState([])
   const [isOpen, ModalControlHandler] = useModalControl()
-
+  const [addQuestion] = useAddQuestionMutation()
+  const FileRef = useRef()
   const onChangeHandler = (e) => {
     const { name, value } = e.target
     setUserValue({
@@ -37,58 +40,60 @@ const index = () => {
 
   const uploadThumbnail = (e) => {
     const fileList = e.target.files
-    //다영이 =>
+    console.log(e.target)
 
     if (imageFile.length > 1) {
       let currentImages = imageFile.slice(1, 2)
       currentImages.push(URL.createObjectURL(fileList[fileList.length - 1]))
       setImageFile(currentImages)
-
-      let currentServerImages = serverFile.slice(1, 2)
-      currentServerImages.push(
-        URL.createObjectURL(fileList[fileList.length - 1]),
-      )
-      setServerFile(currentServerImages)
     } else {
       setImageFile([
         ...imageFile,
         URL.createObjectURL(fileList[fileList.length - 1]),
       ])
-      setServerFile([...serverFile, fileList[fileList.length - 1]])
     }
   }
   const AddQuestionHandler = () => {
     if (!userValue.title || !userValue.content) {
       ModalControlHandler()
     }
-
-    let formData = new formData()
-    formData.append('text', userValue)
-    formData.append('file', imageFile)
-    new Blob([JSON.stringify(userValue)], {
-      type: 'multipart/formdata',
-    }),
-      console.log(formData)
+    const fileList = FileRef.current.files
+    let formData = new FormData() // formData 객체를 생성한다.
+    for (let i = 0; i < fileList.length; i++) {
+      formData.append('images', fileList[i]) // 반복문을 활용하여 파일들을 formData 객체에 추가한다
+    }
+    formData.append('type', userValue.type)
+    formData.append('title', userValue.title)
+    formData.append('content', userValue.content)
+    formData.append('privateYn', userValue.privateYn)
+    formData.append('password', userValue.password)
+    console.log(formData.append)
+    //api호출 로직 짜기
   }
 
   const removeThumbnail = (idx) => {
-    const currentImages = imageFile.splice(idx, 1)
-    setImageFile(currentImages)
+    idx === 1 ? setImageFile(imageFile.splice(idx, 1)) : setImageFile([])
+    console.log('실행!')
   }
 
   const showImage = useMemo(() => {
     return (
       <>
         {imageFile?.map((item, idx) => (
-          <div
-            className="relative w-[86px] h-[86px] bg-cover rounded overflow-hidden"
-            style={{
-              backgroundImage: `url(${imageFile[idx]})`,
-            }}
-            onClick={removeThumbnail}
-          >
-            <div className="absolute right-1 top-1">
-              <CloseIcon size="15" />
+          <div className="relative flex gap-2">
+            <div
+              className="relative w-[86px] h-[86px] bg-cover rounded overflow-hidden shawdow-md"
+              style={{
+                backgroundImage: `url(${imageFile[idx]})`,
+              }}
+              onClick={() => removeThumbnail(idx)}
+            ></div>
+            <div className="absolute -right-2 -top-2">
+              <CloseIcon
+                fill="#000"
+                size="8"
+                className="bg-black-200 p-1 rounded-full"
+              />
             </div>
           </div>
         ))}
@@ -109,7 +114,12 @@ const index = () => {
           onChangeCheckedHandler={onChangeCheckedHandler}
         />
         <Content count={count} onChangeHandler={onChangeHandler} />
-        <AddPicture uploadThumbnail={uploadThumbnail} showImage={showImage} />
+        <AddPicture
+          uploadThumbnail={uploadThumbnail}
+          showImage={showImage}
+          ref={FileRef}
+          count={imageFile.length === 0 ? 0 : imageFile.length}
+        />
         <QnABtn onClick={AddQuestionHandler} />
       </div>
       {isOpen && (
--- src/store/api/questionSlice.js
+++ src/store/api/questionSlice.js
@@ -9,12 +9,29 @@ export const questionApi = createApi({
   endpoints: (builder) => ({
     //카드 상품 불러오기
     getQuestions: builder.query({
+      // query: () => '/customers/question',
       query: () => 'question',
     }),
     getQustion: builder.query({
       query: (id) => `question/${id}`,
     }),
+    addQuestion: builder.mutation({
+      query: ({ data }) => ({
+        url: `customers/questions`,
+        method: 'PUT',
+        // mode: 'cors',
+        headers: {
+          'Content-Type': 'multipart/form-data',
+          authorization: `Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxIiwiYXV0aCI6IlJPTEVfVVNFUiIsImV4cCI6MTY2NDk4MTgxMn0.RulW7VIPZJV4szYO-RpxYBdz8JevLATTxtmz5ePM6_habCD87zEIf0rnTFszib0zDgXptnTZ9CEEIq9ywDMUcQ`,
+        },
+        body: data,
+      }),
+    }),
   }),
 })
 
-export const { useGetQuestionsQuery, useGetQustionQuery } = questionApi
+export const {
+  useGetQuestionsQuery,
+  useGetQustionQuery,
+  useAddQuestionMutation,
+} = questionApi
