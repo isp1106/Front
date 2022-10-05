7c8826906ab52a168bf4590c116910d3f98fa0ed
0seo8 <96939562+0seo8@users.noreply.github.com>
2022-10-06 04:57:59 +0900

    fix: 디테일 페이지 디자인 수정

--- src/components/Product/Detail/Brand.jsx
+++ src/components/Product/Detail/Brand.jsx
@@ -1,24 +1,33 @@
 import React from 'react'
 import { detailProducts } from '../../../dummy/detail'
-import { ReactComponent as GoBackIcon } from '/public/assets/back-on.svg'
+import BackIcon from '../../common/BackIcon'
 import HeartIcon from '../../common/HeartIcon'
 const Brand = () => {
   const { brandImg, brand, brandKo, liked } = detailProducts
   return (
-    <div className="w-full px-5 mt-2">
+    <div className="w-full px-5 mt-2 py-[36px]">
       <div className="flex w-full border border-primary">
         <div className="flex items-center justify-center px-3 py-4 border-primary border-r">
           <img src={brandImg} width="58px" />
         </div>
-        <div className="flex-grow py-3">
-          <div className="px-3 flex items-center w-full justify-between">
+        <div className="flex-grow py-5 px-4">
+          <div className="relative flex flex items-center w-full justify-between">
             <div className="w-full flex items-center">
               <span className="text-color-800 text-sm font-bold">{brand}</span>
-              <GoBackIcon className="rotate-180 w-[14px] h-[22px] ml-1" />
+              <BackIcon
+                className="rotate-180 ml-1"
+                size="14"
+                fill="var(--primary)"
+              />
             </div>
-            <HeartIcon size="22px" off={!liked} />
+            <HeartIcon
+              size="22px"
+              off={liked}
+              fill="var(--primary)"
+              className="absolute right-5 top-[10px]"
+            />
           </div>
-          <h2 className="pl-3 mt-2 font-bold">{brandKo}</h2>
+          <h2 className="text-sm mt-2 font-bold">{brandKo}</h2>
         </div>
       </div>
     </div>
--- src/components/Product/Detail/index.jsx
+++ src/components/Product/Detail/index.jsx
@@ -1,12 +1,13 @@
 import React, { useState } from 'react'
 import MainSlide from './MainSlide'
 import Explanation from './Explanation'
-import Brand from './Brand'
 import DetailDesc from './DetailDesc'
 import Relation from './Magazine'
 import SubContent from './SubContent'
 import DetailBtn from './DetailBtn'
-
+// import Brand from '../../Magazine/Brand'
+import Brand from './Brand'
+import { detailProducts } from '../../../dummy/detail'
 const Detail = () => {
   return (
     <>
@@ -18,7 +19,6 @@ const Detail = () => {
       <Relation />
       <div className="w-full h-[10px] bg-white-200 my-8"></div>
       <SubContent />
-
       <DetailBtn />
     </>
   )
