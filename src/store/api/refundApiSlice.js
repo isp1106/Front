9e94f349590c4d9b2204b68cc3783979327ad8e7
YANGDAYOUNG <jc1525@hanmail.net>
2022-10-09 21:45:26 +0900

    #113 feat: 환불 상품 비동기

---
+++ src/store/api/refundApiSlice.js
@@ -0,0 +1,22 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+export const refundApi = createApi({
+  reducerPath: 'refundApi',
+  // baseQuery,
+  tagTypes: ['Refunds'],
+  baseQuery: fetchBaseQuery({
+    baseUrl: import.meta.env.VITE_BASE_URL,
+  }),
+  endpoints: (builder) => ({
+    //환불 상품 불러오기
+    getRefundItems: builder.query({
+      query: () => 'refund',
+      providesTags: (result, error, arg) =>
+        result
+          ? [...result.map(({ id }) => ({ type: 'Refunds', id })), 'Refunds']
+          : ['Refunds'],
+    }),
+  }),
+})
+
+export const { useGetRefundItemsQuery } = refundApi
