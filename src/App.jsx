f3b3b1c9c7069b05fb8cf1a3daadabbfb5940a42
0seo8 <96939562+0seo8@users.noreply.github.com>
2022-10-11 01:19:02 +0900

    chore: 배포테스트

--- src/App.jsx
+++ src/App.jsx
@@ -45,7 +45,7 @@ import UserEditInfo from './pages/My/UserEditInfo'
 import Refund from './pages/My/Refund'
 import RefundListPage from './pages/My/Refund/RefundListPage'
 import RefundDetailPage from './pages/My/Refund/RefundDetailPage'
-// import NotFoundPage from './pages/NotfoundPage'
+import NotFoundPage from './pages/NotfoundPage'
 
 function App() {
   const location = useLocation()
@@ -106,7 +106,7 @@ function App() {
             <Route path="" element={<Order />} />
             <Route path="completed" element={<CompletedOrder />} />
           </Route>
-          {/* <Route path="/*" element={<NotFoundPage />} /> */}
+          <Route path="/*" element={<NotFoundPage />} />
         </Routes>
         {(!currentPath.includes('/signup') ||
           currentPath !== '/login' ||
