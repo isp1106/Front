4dbba6542088f8eb0afdca918fa1aa8957f11222
0seo8 <96939562+0seo8@users.noreply.github.com>
2022-10-13 05:33:11 +0900

    fix: 위로가기 버튼

--- src/components/Product/Detail/index.jsx
+++ src/components/Product/Detail/index.jsx
@@ -8,39 +8,15 @@ import Relation from './Magazine'
 import SubContent from './SubContent'
 import DetailBtn from './DetailBtn'
 import Brand from './Brand'
-import { cls } from '../../../utils'
-import BackIcon from '../../common/BackIcon'
 import Loading from '../../layout/Loading'
 import ErrorCom from '../../common/ErrorCom'
 import { useDispatch } from 'react-redux'
+import GoTop from '../../common/GoTop'
 
 const Detail = () => {
   const params = useParams()
   const dispatch = useDispatch()
   const { data: list, isLoading, isError } = useGetProductQuery(params.id)
-  const [showButton, setShowButton] = useState(false)
-
-  const scrollToTop = () => {
-    window.scroll({
-      top: 0,
-      behavior: 'smooth',
-    })
-  }
-
-  useEffect(() => {
-    const handleShowButton = () => {
-      if (window.scrollY > 2000) {
-        setShowButton(true)
-      } else {
-        setShowButton(false)
-      }
-    }
-
-    window.addEventListener('scroll', handleShowButton)
-    return () => {
-      window.removeEventListener('scroll', handleShowButton)
-    }
-  }, [])
 
   return (
     <>
@@ -60,15 +36,7 @@ const Detail = () => {
             <div className="w-full h-[10px] bg-white-200 my-8"></div>
             <SubContent />
             <DetailBtn />
-            <div
-              className={cls(
-                'flex items-center justify-center fixed  right-3 text-white bottom-[120px] z-10 w-[38px] h-[38px] rounded-full bg-point font-bold transition-opacity ease-in',
-                showButton ? 'opacity-1' : 'opacity-0',
-              )}
-              onClick={scrollToTop}
-            >
-              <BackIcon className="rotate-90" size="20px" fill="#fff" />
-            </div>
+            <GoTop />
           </div>
         )
       )}
