6b83de32c9d91d9f94b850bff50fbfb5629db78c
0seo8 <96939562+0seo8@users.noreply.github.com>
2022-10-18 22:11:11 +0900

    feat: 마이페이지 token유무에 따른 조건부렌더링 설정

--- src/components/My/MyPage/MyNav.jsx
+++ src/components/My/MyPage/MyNav.jsx
@@ -1,28 +1,38 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import { useCookies } from 'react-cookie'
+
 const MyNav = () => {
+  const [cookies, setCookie, removeCookie] = useCookies()
+  const token = cookies.accessToken
+  const navigate = useNavigate()
+  const onClickHandler = (data) => {
+    token ? navigate(`${data}`) : navigate('/login')
+  }
+  const goToOrderCheck = () => {
+    token ? navigate('order-list') : navigate('/order-check')
+  }
+  const goToRefundList = () => {
+    token ? navigate('refund-list') : navigate('/order-check')
+  }
   return (
     <div className="relative font-medium mt-14 px-5 pt-11 before:content-[attr(before)] before:w-full before:h-2.5 before:bg-white-200 before:absolute before:left-0 before:top-0">
       <h2 className="text-sm text-black-200 mb-1.5">주문정보</h2>
       <ul className="flex flex-wrap">
-        <li className="text-sm w-1/2 py-1">
-          <Link to="order-list" className="font-medium">
-            주문/배송/픽업 조회
-          </Link>
-          {/* Temporary link */}
+        <li className="text-sm w-1/2 py-1 font-medium" onClick={goToOrderCheck}>
+          주문/배송/픽업 조회
         </li>
-        <li className="text-sm w-1/2 py-1">
-          <Link to="refund-list" className="font-medium">
-            취소/교환/환불 내역
-          </Link>
+        <li className="text-sm w-1/2 py-1 font-medium" onClick={goToRefundList}>
+          취소/교환/환불 내역
         </li>
       </ul>
       <h2 className="text-sm text-black-200 mt-9 mb-1.5">고객센터</h2>
       <ul className="flex flex-wrap">
-        <li className="text-sm w-1/2 py-1">
-          <Link to="/my/qna" className="font-medium">
-            1:1 문의
-          </Link>
+        <li
+          className="text-sm w-1/2 py-1 font-medium"
+          onClick={() => onClickHandler('qna')}
+        >
+          1:1 문의
         </li>
         <li className="text-sm w-1/2 py-1">
           <Link to="/faq" className="font-medium">
